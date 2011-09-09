24d45804efc67da374e860e47cd0dd7a186f9b7a
ExternalReality <ericclaudejones@gmail.com>
2011-09-09 01:19:49 +0000

    More restructuring.

--- src/Database/SednaDB/Connection.hs
+++ src/Database/SednaDB/Connection.hs
@@ -4,14 +4,66 @@ import Foreign
 import Foreign.Ptr
 import Foreign.C.Types
 import Foreign.C.String
+import Foreign.Marshal.Alloc
 
+import Control.Monad
+import Control.Monad.Reader
 import Database.SednaDB.Internal.Sedna
 
-sednaConnect :: IO Int
-sednaConnect = alloca $ \conn -> do
-               url      <- newCString "localHost" 
-               dbname   <- newCString "test" 
-               login    <- newCString "" 
-               password <- newCString "" 
-               status   <- c'SEconnect conn url dbname login password
-               return (fromIntegral status)
+
+type SednaConnection = Ptr C'SednaConnection
+
+sednaConnect :: String -> String -> String -> String -> IO SednaConnection
+sednaConnect url dbname login password  =  
+    do
+      conn      <- malloc
+      cUrl      <- newCString url 
+      cDbname   <- newCString dbname 
+      cLogin    <- newCString login
+      cPassword <- newCString password 
+      status    <- c'SEconnect conn 
+                               cUrl 
+                               cDbname 
+                               cLogin 
+                               cPassword
+
+      mapM_ free [cUrl,cDbname,cLogin,cPassword]
+      return conn
+      
+withSednaConnection :: (SednaConnection ->  IO CInt) -> SednaConnection -> IO Int
+withSednaConnection sednaAction conn = 
+  do 
+    resultCode <- sednaAction $ conn
+    return $ fromIntegral resultCode
+    
+sednaCloseConnection :: SednaConnection -> IO Int 
+sednaCloseConnection = withSednaConnection c'SEclose 
+
+sednaBegin :: SednaConnection -> IO Int
+sednaBegin = withSednaConnection c'SEbegin
+
+sednaRollback :: SednaConnection -> IO Int
+sednaRollback = withSednaConnection c'SErollback
+
+sendaCommit :: SednaConnection -> IO Int
+sendaCommit = withSednaConnection c'SEcommit
+
+sednaExecuteAction :: (SednaConnection -> CString -> IO CInt) -> SednaConnection -> String -> IO Int 
+sednaExecuteAction sednaQueryAction conn query = do 
+  resultCode <- withCString query $ sednaQueryAction conn
+  return $ fromIntegral resultCode
+  
+sednaExecuteLong :: SednaConnection -> String -> IO Int
+sednaExecuteLong = sednaExecuteAction c'SEexecuteLong
+
+sednaExecute :: SednaConnection -> String -> IO Int
+sednaExecute = sednaExecuteAction c'SEexecute
+
+
+
+
+  
+                            
+     
+                     
+                      
